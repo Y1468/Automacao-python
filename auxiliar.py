214d59f3e8c01087e88cd6139921bf2938447ebe
Ivan Sousa <azulaoandrade10@gmail.com>
2024-01-19 08:28:17 -0300

    AutomaçãoPython

--- auxiliar.py
+++ auxiliar.py
@@ -3,6 +3,10 @@ import time
 
 
 #Pega a posição de um elemento
+'''
 time.sleep(5)
 print(pyautogui.position())
-   
+'''
+
+time.sleep(5)
+print(pyautogui.position())
--- codigo.py
+++ codigo.py
@@ -1,3 +1,4 @@
+'''
 #Passo a passp dp proeto
 
 #Passo1:Entra no sistema da inpresa
@@ -89,6 +90,69 @@ for linha in tabela.index:
     #Rolando a pagina
 
     pyautogui.scroll(5000)
+
+'''
+import pyautogui
+import time
+
+pyautogui.PAUSE=1
+
+pyautogui.press("win")
+pyautogui.write("edge")
+pyautogui.press("enter")
+
+link="https://dlp.hashtagtreinamentos.com/python/intensivao/login"
+pyautogui.write(link)
+pyautogui.press("enter")
+
+time.sleep(5)
+
+pyautogui.click(x=394, y=357)
+
+pyautogui.write("Ivan sousa de aquino")
+pyautogui.press("tab")
+pyautogui.write("123456")
+pyautogui.click(x=668, y=518)
+time.sleep(3)
+
+import pandas
+
+tabela=pandas.read_csv("produtos.csv")
+print(tabela)
+
+for linha in tabela.index:
+
+    pyautogui.click(x=394, y=244)
+
+    codigo=tabela.loc[linha,"codigo"]
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+
+    pyautogui.write(tabela.loc[linha,"marca"])
+    pyautogui.press("tab")
+
+    pyautogui.write(tabela.loc[linha,"tipo"])
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha,"categoria"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha,"preco_unitario"]))
+    pyautogui.press("tab")
+
+    pyautogui.write(str(tabela.loc[linha,"custo"]))
+    pyautogui.press("tab")
+
+    obs=tabela.loc[linha,"obs"]
+
+    if not pandas.isna(obs):
+        pyautogui.write(obs)
+
+        pyautogui.click(x=589, y=450)
+
+        pyautogui.scroll(5000)
+
+
 
 
 
